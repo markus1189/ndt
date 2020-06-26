56c7d666485ae6279c97744d04eb61546ce24756
Markus Hauck <markus1189@gmail.com>
2020-06-26 16:39:24 +0200

    Allow tracking of dependencies

--- Main.hs
+++ Main.hs
@@ -1,9 +1,18 @@
 module Main where
 
+import Control.Lens.At (at)
+import Control.Lens.Operators
+import qualified Data.Aeson as Aeson
+import Data.Aeson (Value)
+import Data.Aeson.Lens (_Object)
 import Data.Semigroup ((<>))
 import Data.Text (Text)
+import qualified Data.Text as T
+import Network.URI (URI, parseAbsoluteURI)
+import qualified Network.URI as URI
 import Options.Applicative
-import Network.URI (parseAbsoluteURI, URI)
+import System.Exit (ExitCode (..))
+import qualified System.Process.Typed as Process
 
 data Command
   = TrackDependency Dependency
@@ -11,9 +20,15 @@ data Command
 
 data Dependency
   = GithubDependency
-      { githubUrl :: URI,
+      { dependencyKey :: Text,
+        githubUrl :: URI,
         fetchSubmodules :: Bool
       }
+  deriving (Show)
+
+data NdtError
+  = NixPrefetchGitFailed ExitCode
+  | NixPrefetchGitAesonDecodeError String
   deriving (Show)
 
 commandParser :: Parser Command
@@ -23,8 +38,9 @@ trackOptions =
   TrackDependency <$> hsubparser (command "github" (info trackGitHubOptions (progDesc "Track a GitHub repository")))
 
 trackGitHubOptions =
-  GithubDependency <$> argument uriReadM (metavar "GITHUB_URL")
-                   <*> flag False True (long "fetch-submodules" <> help "Fetch submodules")
+  GithubDependency <$> (T.pack <$> argument str (metavar "NAME"))
+    <*> argument uriReadM (metavar "GITHUB_URL")
+    <*> flag False True (long "fetch-submodules" <> help "Fetch submodules")
 
 opts = info (commandParser <**> helper) (fullDesc <> header "Nix Dependency Tracker")
 
@@ -33,11 +49,46 @@ main = do
   options <- execParser opts
   dispatch options
 
+dispatch :: Command -> IO ()
+dispatch (TrackDependency (GithubDependency dependencyKey uri fsm)) = do
+  eitherErrorJson <- nixPrefetchGit uri fsm
+  case eitherErrorJson of
+    Left e -> print e -- TODO display error
+    Right json -> do
+      let (owner, repo) = parseOwnerAndRepo uri
+          json' =
+            json & _Object . at "owner" ?~ Aeson.toJSON owner
+            & _Object . at "repo" ?~ Aeson.toJSON repo
+            & _Object . at "type" ?~ "github"
+      withSources sourcesFilePath (_Object . at dependencyKey ?~ json')
 
-dispatch (TrackDependency (GithubDependency u _)) = print u
+parseOwnerAndRepo :: URI -> (String, String)
+parseOwnerAndRepo uri = (owner, repo)
+  where
+    owner = takeWhile (/= '/') . dropWhile (== '/') . URI.uriPath $ uri -- TODO: extract owner and repo
+    repo = takeWhile (/= '/') . dropWhile (== '/') . dropWhile (/= '/') . dropWhile (== '/') . URI.uriPath $ uri
 
+nixPrefetchGit uri fetchSubmodules = do
+  (ec, stdout) <- Process.readProcessStdout $ Process.proc "nix-prefetch-git" [show uri]
+  return $ case ec of
+    ExitSuccess -> case Aeson.eitherDecode' @Value stdout of
+      Left e -> Left (NixPrefetchGitAesonDecodeError e)
+      Right json -> Right json
+    ExitFailure _ -> Left (NixPrefetchGitFailed ec)
 
 uriReadM = eitherReader parseAbsoluteURI'
-  where parseAbsoluteURI' s = case parseAbsoluteURI s of
-          Nothing -> Left $ "Not an absolute URI: '" <> s <> "'"
-          Just u -> Right u
+  where
+    parseAbsoluteURI' s = case parseAbsoluteURI s of
+      Nothing -> Left $ "Not an absolute URI: '" <> s <> "'"
+      Just u -> Right u
+
+withSources :: FilePath -> (Value -> Value) -> IO ()
+withSources fp f = do
+  decoded <- Aeson.eitherDecodeFileStrict fp
+  case decoded of
+    Left _ -> undefined -- TODO
+    Right deps -> do
+      let deps' = f deps
+      Aeson.encodeFile fp deps'
+
+sourcesFilePath = "sources.json"
