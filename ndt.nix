3faf4b9761543faa6682b1ace30503e33a753f58
Markus Hauck <markus1189@gmail.com>
2020-06-28 10:28:16 +0200

    Overwrite rio version

--- ndt.nix
+++ ndt.nix
@@ -3,7 +3,15 @@
 }:
 
 let
-  haskellPackages = nixpkgs.haskell.packages.${ghc};
+  haskellPackages = nixpkgs.haskell.packages.${ghc}.override {
+    overrides = self: super: {
+      rio = self.callHackageDirect {
+        pkg = "rio";
+        ver = "0.1.17.0";
+        sha256 = "07qxvw168vqv9ma9x4v1g4aiddb6v1cdhl9wa0zbx9980xzpqx81";
+      } {};
+    };
+  };
   ndtDrv = haskellPackages.callCabal2nix "ndt" ./. {};
 in
-  nixpkgs.haskell.lib.shellAware ndtDrv
+  ndtDrv
