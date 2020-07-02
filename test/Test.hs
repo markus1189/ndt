6e61931631879a3b56e66cf517b36c694d5e8d1b
Markus Hauck <markus1189@gmail.com>
2020-07-02 20:27:50 +0200

    Test the fetcher for github

--- test/Test.hs
+++ test/Test.hs
@@ -1,18 +1,12 @@
 module Main where
 
 import Test.Tasty
-import Test.Tasty.Hspec
+import Tests.Fetch (fetchTests)
 
 tests :: IO TestTree
 tests = do
-  ts <- ndtTests
+  ts <- fetchTests
   pure $ testGroup "NDT Tests" [ts]
 
 main :: IO ()
 main = tests >>= defaultMain
-
-ndtTests :: IO TestTree
-ndtTests = testSpec "ndt-hspec" $ do
-  describe "the github fetcher" $ do
-    it "foo" $ do
-      1 `shouldBe` (1::Int)
---
+++ test/Tests/Fetch.hs
@@ -0,0 +1,86 @@
+module Tests.Fetch (fetchTests) where
+
+import           Test.Tasty
+import           Test.Tasty.Hspec
+
+import           Control.Monad.Reader (runReaderT)
+import           Data.Aeson (Value, (.=))
+import qualified Data.Aeson as Aeson
+import           Data.Aeson.Lens (key, _Object, _Bool, _String)
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.HashMap.Strict as HM
+import           Data.Maybe (fromJust)
+import qualified Data.Text as T
+import           Lens.Micro.Platform ((^?), (^?!), lens, to)
+import           Ndt.Fetch (fetchDependency)
+import           Ndt.Types (Dependency(..), HasNixPrefetchGitAction(..), HasNixPrefetchUrlAction(..), NixPrefetchGitArgs(..), NixPrefetchUrlArgs(..))
+import           Network.URI (parseAbsoluteURI)
+
+data TestEnv = TestEnv (NixPrefetchGitArgs -> IO Value) (NixPrefetchUrlArgs -> IO LBS.ByteString)
+
+instance HasNixPrefetchGitAction TestEnv where
+  nixPrefetchGitActionL = lens (\(TestEnv git _) -> git) (\(TestEnv _ url) git -> TestEnv git url)
+
+instance HasNixPrefetchUrlAction TestEnv where
+  nixPrefetchUrlActionL = lens (\(TestEnv _ url) -> url) (\(TestEnv git _) url -> TestEnv git url)
+
+emptyFetchOutput :: TestEnv
+emptyFetchOutput = TestEnv (const (pure (Aeson.object []))) (const (pure ""))
+
+correctFetchOutput :: TestEnv
+correctFetchOutput = TestEnv
+                     (pure . json)
+                     (const (pure "0000000000000000000000000000000000000000000000000000"))
+  where json (NixPrefetchGitArgs _ fsm _) =
+          Aeson.object [ "url" .= ("https://github.com/markus1189/ndt" :: String)
+                       , "rev" .= ("b662443ec1e5152aaf2166a391f4a582117f0fd4" :: String)
+                       , "date" .= ("2020-07-02T20:16:00+02:00" :: String)
+                       , "path" .= ("/nix/store/i3ssqhg14pcsagcb3mgy4c86n72rhnqs-ndt" :: String)
+                       , "sha256" .= ("0v38qqpsagl598n7wbingv3737x7g91xzqci6vzmvfqs8q5wxcbb" :: String)
+                       , "fetchSubmodules" .= fsm
+                       , "deepClone" .= False
+                       , "leaveDotGit" .= False
+                       ]
+
+
+fetchTests :: IO TestTree
+fetchTests = testSpec "fetch" $ do
+  describe "the github fetcher" $ do
+    it "adds owner and repo from the uri" $ do
+      result <- runReaderT (fetchDependency (GithubDependency ndtUri False "master")) emptyFetchOutput
+      result ^? key "owner" `shouldBe` Just "markus1189"
+      result ^? key "repo" `shouldBe` Just "ndt"
+
+    it "remembers the args given" $ do
+      let fetchSubmodules = True
+          branch = "foo"
+      result <- runReaderT (fetchDependency (GithubDependency ndtUri fetchSubmodules branch)) correctFetchOutput
+      result ^? key "fetchSubmodules" . _Bool `shouldBe` Just fetchSubmodules
+      result ^? key "branch" . _String `shouldBe` Just (T.pack branch)
+
+    it "keeps all of the original attributes" $ do
+      result <- runReaderT (fetchDependency (GithubDependency ndtUri False "master")) correctFetchOutput
+      let keys = result ^?! _Object . to HM.keys
+      keys `shouldContainAllOf` [ "url"
+                                , "rev"
+                                , "date"
+                                , "path"
+                                , "sha256"
+                                , "fetchSubmodules"
+                                , "deepClone"
+                                , "leaveDotGit"
+                                ]
+
+    it "adds some custom attributes" $ do
+      result <- runReaderT (fetchDependency (GithubDependency ndtUri False "master")) correctFetchOutput
+      let keys = result ^?! _Object . to HM.keys
+      keys `shouldContainAllOf` [ "owner"
+                                , "repo"
+                                , "type"
+                                , "branch"
+                                ]
+
+  where ndtUri = fromJust $ parseAbsoluteURI "https://github.com/markus1189/ndt"
+
+shouldContainAllOf :: (Show a, Eq a) => [a] -> [a] -> Expectation
+shouldContainAllOf actual expected = actual `shouldSatisfy` (\ks -> all (`elem` ks) expected)
