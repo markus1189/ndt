252e4e20afc9d8c05c1aef9e9f4b54624c40c668
Markus Hauck <markus1189@gmail.com>
2020-07-05 09:48:55 +0200

    Fix filter source

--- ndt.nix
+++ ndt.nix
@@ -12,7 +12,8 @@ let
       } {};
     };
   };
-  ndtSrc = builtins.filterSource (path: type: !(nixpkgs.lib.hasSuffix ".nix" (baseNameOf path))) ./.;
+  excludedFiles = ["ndt.nix" "default.nix" "shell.nix"];
+  ndtSrc = builtins.filterSource (path: type: builtins.elem (baseNameOf path) excludedFiles ) ./.;
   ndtDrv = haskellPackages.callCabal2nix "ndt" ndtSrc {};
 in
   ndtDrv
