fa847c6713a5568c34eb4223757ffd67e3fbc3b7
Markus Hauck <markus1189@gmail.com>
2020-07-05 10:27:58 +0200

    Switch back to manual completion generation

--- default.nix
+++ default.nix
@@ -5,14 +5,24 @@
 let
   drv = import ./ndt.nix { inherit ghc nixpkgs; };
   modifiedDrv = with nixpkgs.haskell.lib;
-    generateOptparseApplicativeCompletion "ndt" (
-      disableExecutableProfiling (
-        disableLibraryProfiling (
-          justStaticExecutables drv)));
+    disableExecutableProfiling (
+      disableLibraryProfiling (
+        justStaticExecutables drv));
   drvWithInputs = modifiedDrv.overrideAttrs (old: {
     buildInputs = old.buildInputs ++ [
       nixpkgs.nix-prefetch-git
     ];
+    nativeBuildInputs = old.nativeBuildInputs ++ [ nixpkgs.installShellFiles ];
+    postInstall = ''
+      $out/bin/ndt --bash-completion-script $out/bin/ndt > ndt.bash
+      installShellCompletion --bash --name ndt.bash ndt.bash
+
+      $out/bin/ndt --zsh-completion-script $out/bin/ndt > ndt.zsh
+      installShellCompletion --zsh --name ndt.zsh ndt.zsh
+
+      $out/bin/ndt --fish-completion-script $out/bin/ndt > ndt.fish
+      installShellCompletion --fish --name ndt.fish ndt.fish
+    '';
   });
   finalDrv = drvWithInputs;
 in
