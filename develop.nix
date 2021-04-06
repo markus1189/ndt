4d8ae931d48bd3dc68c1b830e9a67d92a643f020
Markus Hauck <markus1189@gmail.com>
2021-04-06 20:00:29 +0200

    Set default arg in develop.nix

--- develop.nix
+++ develop.nix
@@ -5,4 +5,4 @@ dontHaddock (
   disableDeadCodeElimination (
     disableExecutableProfiling (
       disableLibraryProfiling (
-        import ./default.nix {}))))
+        import ./default.nix { ghc = "ghc884"; }))))
